c211757ca24152a87bc371bfb27394d2db833c8a
Luke Dunekacke <ldunekacke@gmail.com>
2013-02-25 08:42:25 -0700

    Commented IntBoard.java. Luke's testing commit

--- CluePath/src/Experiment/IntBoard.java
+++ CluePath/src/Experiment/IntBoard.java
@@ -1,7 +1,9 @@
 /* Names: Brendan Casias, Luke Dunekacke
  * 
- * This is the failing tests code
+ * This is the IntBoard Class
  * 
+ *  Here we are assuming that the board is a 4 by 4 square consisting of number 0 to 15
+ *  
  */
 
 package Experiment;
@@ -12,11 +14,13 @@ import java.util.LinkedList;
 import java.util.Map;
 
 public class IntBoard {
+	
 	private Map<Integer, LinkedList<Integer>> adjMtx;
 	private boolean[] visited;
 	private static final int HEIGHT = 4;
 	private static final int WIDTH = 4;
 	
+	// Resets all the varables to defult for tests
 	public IntBoard() {
 		adjMtx = new HashMap();
 		visited = new boolean[HEIGHT * WIDTH];
@@ -25,6 +29,7 @@ public class IntBoard {
 		calcAdjacencies();
 	}
 
+	// This calculates the adjacent squares at a given cell
 	public void calcAdjacencies() {
 		int[][] board = new int[HEIGHT][WIDTH];
 		for(int i = 0; i < HEIGHT; i++)
@@ -45,29 +50,31 @@ public class IntBoard {
 		}
 		
 	}
-	
+	// Make a list of targets from a location on the board
 	public void startTargets(int location, int steps) {
 		
 		
 	}
 	
+	//returns a set of targets
 	public HashSet getTargets() {
 		HashSet returnSet = new HashSet();
 		return returnSet;
 	}
 	
+	// returns the adjacent board locations at a given position on the board
 	public LinkedList<Integer> getAdjList(int number) {
 		
 		return adjMtx.get(number);
 	}
 	
+	// returns the location of the row and column in a linear format
 	public int calcIndex(int row, int column) {
 		return column + row * HEIGHT;
 	}
 
-	
+	// main function Empty for now
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
